067acd922995037d059c00d60fe7f49940041f68
TamiFam <ilya.innain@mail.ru>
2025-09-08 23:40:35 +0300

    build

--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
             address: data.address
           }
           if(user.email && user.displayName) {
-            return axios.post(`https://mma-server-x3l2.onrender.com/new-user`
+            return axios.post(`http://localhost:3000/new-user`
 
 , userImp).then(()=> {
               navigate('/')
@@ -57,7 +57,7 @@ const Register = () => {
   return (
     <div className="flex justify-center items-center pt-14 bg-gray-100">
       <div className='bg-white p-8 rounded-lg shadow-md'>
-        <h2 className='text-3xl font-bold t text-center mb-6'>Please Register</h2>
+        <h2 className='text-2xl font-medium t text-center mb-6'>Register</h2>
         {/*form data */}
         <form  onSubmit={handleSubmit(onSubmit)} >
           <div className='flex items-center gap-5 '>{/*Name  and Email*/}
@@ -99,26 +99,9 @@ const Register = () => {
                   focus:outline-none focus:ring  focus-border-blue-200' />
                 </div>
           </div>
-          <div className='flex items-center gap-5 '>  {/*phone and photoUrl */}
-            <div className='mb-4'>
-              <label htmlFor="phone" className='block text-gray-700 font-bold mb-2'>
-              <AiOutlinePhone className='inline-block mr-2 mb-1 text-lg' />
-              Phone
-                </label>
-                <input type="tel" placeholder='phone' {...register("phone", { required: true })} className='w-full border-gray-300 border rounded-md py-2 px-4 
-                focus:outline-none focus:ring  focus-border-blue-200' />
-                </div>
-                <div className='mb-4'>
-              <label htmlFor="photoUrl" className='block text-gray-700 font-bold mb-2'>
-              <AiOutlinePicture className='inline-block mr-2 mb-1 text-lg' />
-              PhotoURL
-                </label>
-                <input type="url" placeholder='ur photo' {...register("photoURL")} className='w-full border-gray-300 border rounded-md py-2 px-4 
-                focus:outline-none focus:ring  focus-border-blue-200' />
-                </div>
-          </div>
+         
           <div >   {/*gender and address  */}
-          <div className='mb-4'>
+          {/* <div className='mb-4'>
               <label htmlFor="gender" className='block text-gray-700 font-bold mb-2'>
               <AiOutlineUser className='inline-block mr-2 mb-1 text-lg' />
               Gender
@@ -130,13 +113,13 @@ const Register = () => {
         <option value="male">female</option>
         <option value="other">gay(не желетельно)</option>
       </select>
-                </div>
+                </div> */}
                 <div className='mb-4'>
               <label htmlFor="address" className='block text-gray-700 font-bold mb-2'>
               <IoLocationOutline className='inline-block mr-2 mb-1 text-lg' />
               Address
                 </label>
-                <textarea  rows={3} placeholder='Enter Address'{...register("address", {required: true})} className='w-full border border-gray-300
+                <textarea  rows={1}  placeholder='Enter Address'{...register("address", {required: true})} className='w-full border border-gray-300
                  rounded-md py-2 px-4 focus:outline-none focus:ring focus:border-blue-300'>
                  
       </textarea>
@@ -144,7 +127,7 @@ const Register = () => {
                 
           </div>
           <div className='text-center'>
-            <button type='submit' className='  px-4 py-2 bg-secondary hover:bg-red-500 text-white rounded-md  '>Reggister if u not nigga</button>
+            <button type='submit' className='  px-4 py-2 bg-secondary hover:bg-red-500 text-white rounded-md  '>Register</button>
             {
             errors && (<div className='text-red-500 text-sm w-full mt-1'>
               <p>Password doesn't match</p>
@@ -152,7 +135,7 @@ const Register = () => {
           }
           </div>                           
         </form>
-        <p className='text-center text-sm  text-gray-500'> Already have an account?<Link  className='underline' to="/login "> LOGIN</Link>
+        <p className='text-center text-sm  text-gray-500'> Already have an account?<Link  className='underline' to="/login"> LOGIN</Link>
           
         </p>
         <GoogleLogin/>
